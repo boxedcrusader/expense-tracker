6b77b3a1b8ec4b378e9f08e920304f7dbcac0a9c
Muhammed <boxedcrusader@gmail.com>
2025-09-06 17:59:19 +0100

    Removed Railway

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://expense-tracker-production-f1d6.up.railway.app";
+const API_BASE = "http://localhost:3000";
 let token = localStorage.getItem("token") || null;
 
 document.getElementById("signupModal").addEventListener("submit", async (e) => {
